671a5341ecfbdda0281abdd3527911904f70b9a0
Okwoli-Godwin <okwolig60@gmail.com>
2024-07-27 17:09:35 +0100

    update

--- src/components/static/Header.tsx
+++ src/components/static/Header.tsx
@@ -1,7 +1,7 @@
 import img from "../../assets/kaologo.png"
 import { MdMenu } from "react-icons/md";
 import { useState } from "react"
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
 
@@ -16,38 +16,37 @@ const Header = () => {
   }
 
   return (
-    <div className="w-[100%] h-[70px]  flex items-center justify-center fixed bg-[#fff] z-[20]">
+    <div className="w-[100%] h-[70px] flex items-center justify-center fixed  z-[20]">
       <div className="w-[90%] h-[100%] flex items-center justify-between">
-        <NavLink onClick={closeSidebar} to="/" className="ml-[-16px] md:ml-[-0px] sm:ml-[-0px] xsm:ml-[-12px]" style={{textDecoration: "none", background: "none", }}>
-          <img src={img} alt="" className="h-[40px]"/>
-        </NavLink>
-
+          <Link to="/">
+            <img src={img} alt="" className="h-[40px]"/>
+          </Link>
         <div className="flex items-center md:hidden sm:hidden xsm:hidden mr-[-25px]">
           <h3 className="text-[18px] font-[500] mr-[35px] cursor-pointer">Product & Services</h3>
           <NavLink to="/agent" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[18px] font-[500] mr-[35px] cursor-pointer">Agent</h3>
           </NavLink>
           <NavLink to="/about" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[18px] font-[500] mr-[35px] cursor-pointer">About</h3>
           </NavLink>
           <NavLink to="/affiliate" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[18px] font-[500] mr-[35px] cursor-pointer">Affiliate</h3>
           </NavLink>
           <NavLink to="/contact" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[18px] font-[500] mr-[35px] cursor-pointer">Contact us</h3>
@@ -58,33 +57,33 @@ const Header = () => {
       </div>
 
       {show ? (
-        <div className="w-[100%] h-[100vh] absolute bg-[rgba(0,0,0,0.5)] top-[70px] left-0 hidden md:flex sm:flex xsm:flex justify-end">
+        <div onClick={closeSidebar} className="w-[100%] h-[100vh] absolute bg-[rgba(0,0,0,0.5)] top-[70px] left-0 hidden md:flex sm:flex xsm:flex justify-end">
         <div className="w-[30%] sm:w-[45%] xsm:w-[55%] h-[100%] bg-[#fff] pt-[20px] pl-[15px] flex flex-col">
           <h3 className="text-[16px] font-[500] cursor-pointer pb-[20px]">Product & Services</h3>
           <NavLink onClick={closeSidebar} to="/agent" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[16px] font-[500] cursor-pointer pb-[20px]">Agent</h3>
           </NavLink>
           <NavLink onClick={closeSidebar} to="/about" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[16px] font-[500] cursor-pointer pb-[20px]">About</h3>
           </NavLink>
           <NavLink onClick={closeSidebar} to="/affiliate" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[16px] font-[500] cursor-pointer pb-[20px]">Affiliate</h3>
           </NavLink>
           <NavLink onClick={closeSidebar} to="/contact" style={({ isActive }) => ({
-            textDecoration: "none", 
-            background: "none", 
+            textDecoration: "none",
+            background: "none",
             color: isActive ? "#0030AD" : ""
           })}>
             <h3 className="text-[16px] font-[500] cursor-pointer pb-[20px]">Contact us</h3>
@@ -97,3 +96,32 @@ const Header = () => {
 }
 
 export default Header
+
+// import img from "../../assets/kaologo.png"
+// import { NavLink } from "react-router-dom";
+// import {useState} from "react"
+
+// const Header = () => {
+
+//   const [show, setShow] = useState(false)
+
+//    const ToggleShow = () => {
+//      setShow(!show)
+//   }
+
+//    const closeSidebar = () => {
+//      setShow(false)
+//    }
+
+//   return (
+//     <div className="w-[100%] h-[70px] bg-[red] flex justify-center items-center fixed z-[20]">
+//       <div className="w-[90%] h-[100%] bg-[green] flex justify-center items-center">
+//         {/* <NavLink onClick={closeSidebar} to="/" className="" style={{textDecoration: "none", background: "none", }}> */}
+//           <img src={img} alt="" className="h-[40px]" />
+//         {/* </NavLink> */}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Header
