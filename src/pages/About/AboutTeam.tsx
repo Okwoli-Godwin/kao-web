04d29696afbedc9c3636ac679374e8dbc8acfdf0
Okwoli-Godwin <okwolig60@gmail.com>
2024-07-31 05:12:45 +0100

    update

--- src/pages/About/AboutTeam.tsx
+++ src/pages/About/AboutTeam.tsx
@@ -1,76 +1,37 @@
-import img from "../../assets/Frame 598.png"
-import img2 from "../../assets/Frame 599.png"
-import img3 from "../../assets/Frame 600.png"
-import img4 from "../../assets/Frame 601.png"
+import img from "../../assets/Frame 598.png";
+import img2 from "../../assets/Frame 599.png";
+import img3 from "../../assets/Frame 600.png";
+import img4 from "../../assets/Frame 601.png";
 
 const AboutTeam = () => {
   return (
-    <div className="w-[100%] flex justify-center mt-[100px] md:mt-[70px] sm:mt-[10px] xsm:mt-[-65px] flex-col items-center">
-        <div className="w-[90%] flex sm:mt-[120px] xsm:mt-[130px]">
-            <h1 className="text-[35px] sm:text-[27px] xsm:text-[25px] font-[600] w-[870px] sm:w-[100%] xsm:w-[100%]">
-                <span className="text-[#0030AD]">Meet our dynamic team</span> dedicated to ensuring a seamless shopping experience:
-            </h1>
-        </div>
+    <div className="w-full flex justify-center mt-24 md:mt-18 sm:mt-[-12] xsm:mt-[40px] flex-col items-center">
+      <div className="w-[90%] flex sm:mt-[30px] xsm:mt-[32px]">
+        <h1 className="text-[35px] sm:text-[27px] xsm:text-[25px] font-[600] w-[870px] sm:w-full xsm:w-full">
+          <span className="text-[#0030AD]">Meet our dynamic team</span> dedicated to ensuring a seamless shopping experience:
+        </h1>
+      </div>
 
-        <div className="w-[100%] md:w-[90%] sm:w-[90%] xsm:w-[90%] md:flex-wrap sm:flex-wrap xsm:flex-wrap md:justify-between flex h-[300px] md:h-[100%] sm:h-[100%] xsm:h-[100%] mt-[40px]">
-            <div className="w-[20%] sm:h-[320px] xsm:h-[320px] relative md:w-[49%] sm:w-[100%] xsm:w-[100%] md:mb-[30px] sm:mb-[30px] xsm:mb-[30px] h-[100%] bg-[blue]">
-                <div className="absolute h-[135px] w-[270px] bg-[#0030AD] bottom-[15px] rounded-tl-[7px] rounded-br-[7px] right-[10px] border-r-[3px] border-[#DE801C] flex pl-[10px] pr-[10px] pt-[10px] flex-col">
-                  <h4 className="font-[400] text-[#fff] text-[18px] md:text-[15px] sm:text-[15px] xsm:text-[15px]">Ayomide Abin</h4>
-                  <p className="text-[14px] font-[300] text-[#fff]">Founder</p>
-                  <div className="w-[80px] h-[1px] bg-[#fff] mt-[10px]"></div>
-                  <p className="text-[15px] font-[400] text-[#fff] mt-[12px]">
-                    A visionary entrepreneur passionate about revolutionizing e-commerce
-                  </p>
-                </div>
-                <img src={img} alt="" className="w-[100%] h-[100%] object-cover"/>
+      <div className="w-full md:w-[90%] sm:w-[90%] xsm:w-[90%] md:flex-wrap sm:flex-wrap xsm:flex-wrap md:justify-between flex h-[300px] md:h-auto sm:h-auto xsm:h-auto mt-10">
+        {[img, img2, img3, img4, img].map((image, index) => (
+          <div
+            key={index}
+            className="w-[20%] sm:h-[320px] xsm:h-[320px] relative md:w-[49%] sm:w-full xsm:w-full md:mb-8 sm:mb-8 xsm:mb-8 h-full bg-blue group"
+          >
+            <img src={image} alt="" className="w-full h-full object-cover" />
+            <div className="absolute h-[135px] w-[270px] bg-[#0030AD] bottom-4 rounded-tl-[7px] rounded-br-[7px] right-2 border-r-[3px] border-[#DE801C] flex pl-2 pr-2 pt-2 flex-col sm:opacity-100 md:opacity-100 xsm:opacity-100 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              <h4 className="font-[400] text-[#fff] text-[18px] md:text-[15px] sm:text-[15px] xsm:text-[15px]">Ayomide Abin</h4>
+              <p className="text-[14px] font-[300] text-[#fff]">Founder</p>
+              <div className="w-[80px] h-[1px] bg-[#fff] mt-2"></div>
+              <p className="text-[15px] font-[400] text-[#fff] mt-3">
+                A visionary entrepreneur passionate about revolutionizing e-commerce
+              </p>
             </div>
-            <div className="w-[20%] sm:h-[320px] xsm:h-[320px]  md:w-[48%] relative sm:w-[100%] xsm:w-[100%] sm:mb-[30px] xsm:mb-[30px] h-[100%] bg-[blue]">
-                <div className="absolute h-[135px] w-[270px] bg-[#0030AD] bottom-[15px] rounded-tl-[7px] rounded-br-[7px] right-[10px] border-r-[3px] border-[#DE801C] flex pl-[10px] pr-[10px] pt-[10px] flex-col">
-                  <h4 className="font-[400] text-[#fff] text-[18px] md:text-[15px] sm:text-[15px] xsm:text-[15px]">Ayomide Abin</h4>
-                  <p className="text-[14px] font-[300] text-[#fff]">Founder</p>
-                  <div className="w-[80px] h-[1px] bg-[#fff] mt-[10px]"></div>
-                  <p className="text-[15px] font-[400] text-[#fff] mt-[12px]">
-                    A visionary entrepreneur passionate about revolutionizing e-commerce
-                  </p>
-                </div>
-                <img src={img2} alt="" className="w-[100%] h-[100%] object-cover"/>
-            </div>
-            <div className="w-[20%] relative sm:h-[320px] xsm:h-[320px] md:w-[48%] sm:w-[100%] xsm:w-[100%] sm:mb-[30px] xsm:mb-[30px] h-[100%] bg-[blue]">
-                <img src={img3} alt="" className="w-[100%] h-[100%] object-cover"/>
-                <div className="absolute h-[135px] w-[270px] bg-[#0030AD] bottom-[15px] rounded-tl-[7px] rounded-br-[7px] right-[10px] border-r-[3px] border-[#DE801C] flex pl-[10px] pr-[10px] pt-[10px] flex-col">
-                  <h4 className="font-[400] text-[#fff] text-[18px] md:text-[15px] sm:text-[15px] xsm:text-[15px]">Ayomide Abin</h4>
-                  <p className="text-[14px] font-[300] text-[#fff]">Founder</p>
-                  <div className="w-[80px] h-[1px] bg-[#fff] mt-[10px]"></div>
-                  <p className="text-[15px] font-[400] text-[#fff] mt-[12px]">
-                    A visionary entrepreneur passionate about revolutionizing e-commerce
-                  </p>
-                </div>
-            </div>
-            <div className="w-[20%] relative sm:h-[320px] xsm:h-[320px] md:w-[48%] sm:w-[100%] xsm:w-[100%] sm:mb-[30px] xsm:mb-[30px] h-[100%] bg-[blue]">
-                  <img src={img4} alt="" className="w-[100%] h-[100%] object-cover" />
-                  <div className="absolute h-[135px] w-[270px] bg-[#0030AD] bottom-[15px] rounded-tl-[7px] rounded-br-[7px] right-[10px] border-r-[3px] border-[#DE801C] flex pl-[10px] pr-[10px] pt-[10px] flex-col">
-                  <h4 className="font-[400] text-[#fff] text-[18px] md:text-[15px] sm:text-[15px] xsm:text-[15px]">Ayomide Abin</h4>
-                  <p className="text-[14px] font-[300] text-[#fff]">Founder</p>
-                  <div className="w-[80px] h-[1px] bg-[#fff] mt-[10px]"></div>
-                  <p className="text-[15px] font-[400] text-[#fff] mt-[12px]">
-                    A visionary entrepreneur passionate about revolutionizing e-commerce
-                  </p>
-                </div>
-            </div>
-            <div className="w-[20%] relative sm:h-[320px] xsm:h-[320px] md:w-[48%] sm:w-[100%] xsm:w-[100%] sm:mb-[30px] xsm:mb-[30px] h-[100%] bg-[blue] md:mt-[22px]">
-                <img src={img} alt="" className="w-[100%] h-[100%] object-cover"/>
-                <div className="absolute h-[135px] w-[270px] bg-[#0030AD] bottom-[15px] rounded-tl-[7px] rounded-br-[7px] right-[10px] border-r-[3px] border-[#DE801C] flex pl-[10px] pr-[10px] pt-[10px] flex-col">
-                  <h4 className="font-[400] text-[#fff] text-[18px] md:text-[15px] sm:text-[15px] xsm:text-[15px]">Ayomide Abin</h4>
-                  <p className="text-[14px] font-[300] text-[#fff]">Founder</p>
-                  <div className="w-[80px] h-[1px] bg-[#fff] mt-[10px]"></div>
-                  <p className="text-[15px] font-[400] text-[#fff] mt-[12px]">
-                    A visionary entrepreneur passionate about revolutionizing e-commerce
-                  </p>
-                </div>
-            </div>
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutTeam
+export default AboutTeam;
